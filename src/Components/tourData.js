b8c6ac18cdd5ab4e4d4d32810ddb6166af8196d7
Chousin Rahit <ChousinRahit@users.noreply.github.com>
2019-04-04 08:57:42 +0530

    city information changed

--- src/Components/tourData.js
+++ src/Components/tourData.js
@@ -5,7 +5,7 @@ export const tourData = [
     img: "./img/newyork.jpeg",
     name: "new york bridge tour",
     info:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel,repellendus!"
+      "New York City is home to over 2,000 bridges and tunnels. Several agencies manage this network of crossings, including the New York City Department of Transportation, Metropolitan Transportation Authority, Port Authority of New York and New Jersey, New York State Department of Transportation and Amtrak."
   },
   {
     id: 2,
@@ -13,7 +13,7 @@ export const tourData = [
     img: "./img/paris.jpeg",
     name: "paris lights tour",
     info:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel,repellendus!"
+      "Discover the magic of Paris at night with a leisurely evening walking tour. Stroll along the romantic riverbanks of the Seine, admire the glowing glass pyramid in the courtyard of the Louvre and experience the vibrant nocturnal energy of Saint-Germain-des-Prés. Learn about life in the city and get recommendations on the best bars, cafés and nightspots from your local Parisian guide. Numbers are limited to 12 people for an intimate, small-group tour."
   },
   {
     id: 3,
@@ -21,7 +21,7 @@ export const tourData = [
     img: "./img/london.jpeg",
     name: "london royal palace tour",
     info:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel,repellendus!"
+      "Widely considered the finest and most beautiful residences in the United Kingdom, London's royal palaces attract admirers from around the world. Here the top spots to see how the British royal family lives on your next trip to this capital city."
   },
   {
     id: 4,
@@ -29,6 +29,6 @@ export const tourData = [
     img: "./img/tokyo.jpeg",
     name: "tokyo sushi tour",
     info:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel,repellendus!"
+      "Enjoy the perfect sushi-lover’s experience during this visit to the world-famous Tsukiji Fish Market, followed by a sushi-making class led by master instructor and lunch. Get lost in the labyrinth of seafood stalls with your guide at the world’s largest seafood market, then head to your cooking workshop, where you’ll learn to make sushi favorites including rolls and nigiri alongside your class of Tokyo locals. Your combined Tsukiji and sushi-making class also includes subway transport to the market."
   }
 ];
